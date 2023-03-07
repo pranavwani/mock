eec91ddf1ec28d95f2c96b2eb2e0deb51f0c63e8
Pranav Wani <pranavanilkumar.wani@hcl.com>
2023-03-07 16:12:54 +0530

    get API added

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const app = express();
 
 app.use(express.json());
 
+const records = []
+
 app.post('/', (req, res) => {
   if (!req.body.title) return res.status(500).send(req.body);
 
   res.json(req.body);
 });
+
+app.get('/', (req, res) => {
+  res.json(records)
+})
 
 app.listen(process.env.port || 8547);
